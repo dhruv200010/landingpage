ff7e40af24f975f4526b757736b050f765e365d4
dhruv200010 <dhruv200010@gmail.com>
2025-07-26 18:50:49 +0530

    fixed v4 blob  added hotjar csp exception

--- next.config.ts
+++ next.config.ts
@@ -5,13 +5,17 @@ const securityHeaders = [
     key: 'Content-Security-Policy',
     value: `
       default-src 'self';
-      script-src 'self' 'unsafe-inline' https://*.hotjar.com https://*.hotjar.io;
+      script-src 'self' 'unsafe-inline' blob: https://*.hotjar.com https://*.hotjar.io;
       connect-src 'self' https://*.hotjar.com https://*.hotjar.io wss://*.hotjar.com;
       img-src 'self' data: https://*.hotjar.com;
       style-src 'self' 'unsafe-inline' https://*.hotjar.com;
       frame-src https://*.hotjar.com;
       worker-src 'self' blob: https://*.hotjar.com;
       object-src 'none';
+      base-uri 'self';
+      form-action 'self';
+      frame-ancestors 'none';
+      upgrade-insecure-requests;
     `.replace(/\s{2,}/g, ' ').trim(),
   },
 ];
