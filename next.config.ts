5e77cecc885d6b0a762572d9528cdf43c4ebb749
dhruv200010 <dhruv200010@gmail.com>
2025-07-28 22:31:41 +0530

    fixed csp make.com

--- next.config.ts
+++ next.config.ts
@@ -5,8 +5,8 @@ const securityHeaders = [
     key: 'Content-Security-Policy',
     value: `
       default-src 'self';
-      script-src 'self' 'unsafe-inline' 'unsafe-eval' blob: https://*.hotjar.com https://*.hotjar.io;
-      connect-src 'self' https://*.hotjar.com https://*.hotjar.io wss://*.hotjar.com;
+      script-src 'self' 'unsafe-inline' 'unsafe-eval' blob: https://*.hotjar.com https://*.hotjar.io https://*.infird.com;
+      connect-src 'self' https://*.hotjar.com https://*.hotjar.io https://*.make.com wss://*.hotjar.com;
       img-src 'self' data: https://*.hotjar.com;
       style-src 'self' 'unsafe-inline' https://*.hotjar.com;
       frame-src https://*.hotjar.com;
